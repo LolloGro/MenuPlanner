1cb64428e636bea4910ef41e1b530e887ae95fd3
Louise <emmalouisekarlsson@hotmail.com>
2025-12-28 16:55:28 +0100

    Entity for Meal and Recipe

---
+++ src/main/java/com/lollo/menuplanner/entity/Meal.java
@@ -0,0 +1,93 @@
+package com.lollo.menuplanner.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "meal")
+public class Meal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Integer id;
+    @Column(nullable = false)
+    private String mealName;
+    @Column(nullable = false)
+    private String mainIngredient;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private MealType mealType;
+    @Column(nullable = false)
+    private int time;
+    @OneToOne(optional = true, cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "recipe_id", unique = true, nullable = true)
+    private Recipe recipe;
+
+    public Meal() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMealName() {
+        return mealName;
+    }
+
+    public void setMealName(String mealName) {
+        this.mealName = mealName;
+    }
+
+    public String getMainIngredient() {
+        return mainIngredient;
+    }
+
+    public void setMainIngredient(String mainIngredient) {
+        this.mainIngredient = mainIngredient;
+    }
+
+    public MealType getMealType() {
+        return mealType;
+    }
+
+    public void setMealType(MealType mealType) {
+        this.mealType = mealType;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Meal meal = (Meal) o;
+        return getId() != null && Objects.equals(getId(), meal.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+}
