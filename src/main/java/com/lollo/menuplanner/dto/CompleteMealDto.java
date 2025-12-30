8c616ebe78731e6e632e18763068fcbcee3c6684
Louise <emmalouisekarlsson@hotmail.com>
2025-12-30 17:14:57 +0100

    Removed recipe since it will be managed in a separate feature to come.

--- src/main/java/com/lollo/menuplanner/dto/CompleteMealDto.java
+++ src/main/java/com/lollo/menuplanner/dto/CompleteMealDto.java
@@ -1,14 +1,12 @@
 package com.lollo.menuplanner.dto;
 
 import com.lollo.menuplanner.entity.MealType;
-import com.lollo.menuplanner.entity.Recipe;
 
 public record CompleteMealDto(
     Integer id,
     String mealName,
     String mainIngredient,
     MealType mealType,
-    Integer time,
-    Recipe recipe
+    Integer time
 ) {
 }
