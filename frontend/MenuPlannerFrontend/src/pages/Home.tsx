c0d493099c908dd69b08a79b1c50754175b7bc1e
Louise <emmalouisekarlsson@hotmail.com>
2026-01-15 20:35:24 +0100

    Updated title, tailwind config and index.css.

--- frontend/MenuPlannerFrontend/src/pages/Home.tsx
+++ frontend/MenuPlannerFrontend/src/pages/Home.tsx
@@ -1,9 +1,25 @@
+import Layout from "../components/Layout.tsx";
+import Box from "../components/Box.tsx";
+
 export default function Home(){
     return (
-        <>
-        <div>
-            <h1 className="text-3xl">Menu Planner</h1>
-        </div>
-        </>
+            <Layout>
+                <section className="max-w-xl min-w-xs mt-4 mb-4">
+                        <h1 className="text-3xl font-bold mb-2">What's for dinner?</h1>
+                        <p className="mb-2"> We help you remember meals you might otherwise forget
+                            and make meal planning a little easier.</p>
+                        <p> Here you can gather all your favorite dishes and recipes in one place.
+                            Create weekly meal plans based on what you actually enjoy eating —
+                            either on your own or by letting chance decide.</p>
+                </section>
+                <section className="max-w-xl min-w-xs flex flex-row gap-4 flex-wrap mb-4">
+                    <Box href={"/meals"}>
+                        <p>Add a meal</p>
+                    </Box>
+                    <Box href={"/menus"}>
+                        <p>A Create a menu</p>
+                    </Box>
+                </section>
+            </Layout>
     )
 }
