2f14a741de21350c288b9c4ef9802dc1cc1cce10
Louise <emmalouisekarlsson@hotmail.com>
2026-01-17 21:32:03 +0100

    Added route for meals

--- frontend/MenuPlannerFrontend/src/App.tsx
+++ frontend/MenuPlannerFrontend/src/App.tsx
@@ -1,11 +1,13 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import Home from "./pages/Home.tsx";
+import Home from "./pages/Home";
+import Meals from "./pages/Meals";
 function App() {
 
   return (
   <BrowserRouter>
       <Routes>
           <Route path="/" element={<Home/>} />
+          <Route path="/meals" element={<Meals/>}/>
       </Routes>
   </BrowserRouter>
   )
