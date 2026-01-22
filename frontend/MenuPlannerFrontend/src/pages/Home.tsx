8f903d2c6233bb9fea320d259e0ae7f6793a0f8a
Louise <emmalouisekarlsson@hotmail.com>
2026-01-22 20:10:29 +0100

    Added image on homepage and added styling to view recipe

--- frontend/MenuPlannerFrontend/src/pages/Home.tsx
+++ frontend/MenuPlannerFrontend/src/pages/Home.tsx
@@ -1,10 +1,10 @@
 import Layout from "../components/Layout";
-import Box from "../components/Box";
+import HomePage from "../assets/HomePage250.png"
 
 export default function Home(){
     return (
             <Layout>
-                <section className="max-w-md min-w-xs mt-6 mb-6">
+                <section className="max-w-md min-w-xs mt-6 mb-8">
                         <h1 className="text-3xl font-bold mb-2">What's for dinner?</h1>
                         <p className="mb-2"> We help you remember meals you might otherwise forget
                             and make meal planning a little easier.</p>
@@ -12,13 +12,8 @@ export default function Home(){
                             Create weekly meal plans based on what you actually enjoy eating —
                             either on your own or by letting chance decide.</p>
                 </section>
-                <section className="max-w-xl min-w-xs flex flex-row gap-4 flex-wrap mb-4">
-                    <Box href={"/meals"}>
-                        <p>Add a meal</p>
-                    </Box>
-                    <Box href={"/menus"}>
-                        <p>Create a menu</p>
-                    </Box>
+                <section className="flex flex-col justify-center mb-4">
+                    <img src={HomePage} alt="Menu"/>
                 </section>
             </Layout>
     )
