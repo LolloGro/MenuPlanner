48eebf921acb6a0d31e0634c280b7b9a253c21ba
Louise <emmalouisekarlsson@hotmail.com>
2026-02-05 19:51:33 +0100

    Added input field for text search to filter meals and menus.

--- frontend/MenuPlannerFrontend/src/components/ListOfMenu.tsx
+++ frontend/MenuPlannerFrontend/src/components/ListOfMenu.tsx
@@ -4,12 +4,13 @@ import type {ReadMenu} from "../types/Menu.ts";
 import DefaultButton from "./DefaultButton.tsx";
 import ViewMenu from "./ViewMenu.tsx";
 import Spinner from "./Spinner.tsx";
+import SearchFiled from "./SearchFiled.tsx";
 
 export default function ListOfMenu({onClose}:{onClose:() => void}) {
 
     const {menu, errorViewMenu, loadingViewMenu} = useViewMenu();
     const [viewMenu, setViewMenu] = useState<ReadMenu | null>(null);
-
+    const [searchFilter, setSearchFilter] = useState<String>("");
 
     function handleView(ref:ReadMenu){
         setViewMenu(ref);
@@ -27,6 +28,9 @@ export default function ListOfMenu({onClose}:{onClose:() => void}) {
         return <p>No menus stored yet!</p>
     }
 
+    const filterMenus = menu.filter(m =>
+        m.menuName.toLowerCase().includes(searchFilter.toLowerCase()));
+
     return (
         <div className={"fixed inset-0 bg-black/50 flex items-center justify-center z-50"}>
             <div className={"flex flex-col justify-center max-h-screen min-w-100 bg-white rounded-lg shadow-xl p-4"}>
@@ -34,9 +38,10 @@ export default function ListOfMenu({onClose}:{onClose:() => void}) {
                     <h2 className={"text-2xl"}>List of menus</h2>
                     <DefaultButton type={"button"} text={"Close"} onClick={onClose}/>
                 </div>
+                <SearchFiled inputText={"search menu"} onSearch={setSearchFilter}/>
                 <div className={"min-w-80 max-w-90 overflow-auto"}>
                     <ul>
-                        {menu.map(ref => (
+                        {filterMenus.map(ref => (
                             <li key={ref.id}><DefaultButton type={"button"} text={"View"} onClick={() => handleView(ref)}></DefaultButton>{ref.menuName}</li>
                         ))}
                     </ul>
